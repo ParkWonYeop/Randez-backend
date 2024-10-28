c9fa313cfb3b211fa45b76227377643bebd4ff10
ParkWonYeop <pwy9679@gnu.ac.kr>
2024-10-28 23:37:14 +0900

    2024.10.28/develop
    1. 의존성 버전 명시

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,34 +27,35 @@ repositories {
 
 dependencies {
     //implementation
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.5")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.3.5")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.3.5")
+    implementation("org.springframework.boot:spring-boot-starter:3.3.5")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
     implementation("ch.qos.logback.contrib:logback-jackson:0.1.5")
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 
     //runtimeOnly
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2:2.3.232")
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
 
     //compileOnly
-    compileOnly("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok:1.18.34")
 
     //developmentOnly
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-devtools:3.3.5")
 
     //testImplementation
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.5")
+    testImplementation("org.xmlunit:xmlunit-core:2.10.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.21")
+    testImplementation("org.springframework.security:spring-security-test:6.3.4")
 
     //testRuntimeOnly
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.3")
 
     //annotationProcessor
-    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
 
 kotlin {
